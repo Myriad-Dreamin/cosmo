3fc98d8772b78ac0a5b80c2077ef85f8aadcb7ce
Myriad-Dreamin <camiyoru@gmail.com>
2024-09-18 16:43:31 +0800

    feat: setup cosmo lsp

--- editors/vscode/src/extension.ts
+++ editors/vscode/src/extension.ts
@@ -1,3 +1,10 @@
 import { type ExtensionContext } from "vscode";
+import { activateLsp, deactivateLsp } from "./lsp";
 
-export async function activate(context: ExtensionContext): Promise<void> {}
+export async function activate(context: ExtensionContext): Promise<void> {
+  activateLsp(context);
+}
+
+export function deactivate(): void {
+  deactivateLsp();
+}
