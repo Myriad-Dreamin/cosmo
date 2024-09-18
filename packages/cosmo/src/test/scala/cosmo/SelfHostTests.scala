a3e2a5d752075f6eb2d9babcdd33cbb17ee2ada4
Myriad-Dreamin <camiyoru@gmail.com>
2024-09-18 16:44:45 +0800

    checkpoint 1

--- packages/cosmo/src/test/scala/cosmo/SelfHostTests.scala
+++ packages/cosmo/src/test/scala/cosmo/SelfHostTests.scala
@@ -3,9 +3,6 @@ import scala.scalajs.js
 
 class SelfHostTest extends munit.FunSuite:
   def runTestOnFile(path: String) = {
-    // read the file
-    var src =
-      cosmo.NodeFs.readFileSync(path, "utf8").asInstanceOf[String]
     var compiler = new Cosmo();
     compiler.loadPackage(PackageMetaSource.ProjectPath("library/std"));
     compiler.preloadPackage("std");
@@ -16,7 +13,7 @@ class SelfHostTest extends munit.FunSuite:
     )
   }
 
-  test("CompileDriver/callNode") {
-    runTestOnFile("samples/CompileDriver/callNode.cos")
+  test("parser") {
+    runTestOnFile("packages/cosmoc/src/parser.cos")
   }
 end SelfHostTest
